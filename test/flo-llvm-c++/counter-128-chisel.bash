ca6e5dc03ca65196f4918e89afb77dba2bd20166
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-05 08:13:02 -0800

    Add Counter128 Chisel test
    This actually runs the counter example all the way through Chisel.

---
+++ test/flo-llvm-c++/counter-128-chisel.bash
@@ -0,0 +1,32 @@
+#include "tempdir.bash"
+#include "chisel-jar.bash"
+
+cat >test.scala <<EOF
+import Chisel._
+
+class test extends Module {
+  val io = new Bundle {
+    val o = UInt(OUTPUT, width = 128)
+  }
+
+  val r = Reg(init = UInt(0, width = 128))
+  r := r + UInt(1)
+  io.o := r
+}
+
+class tests(t: test) extends Testy(t) {
+  var cycle = 0
+  do {
+    step(1)
+    cycle += 1
+  } while (cycle < 10)
+}
+
+object test {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(args, () => Module(new test())) { t => new tests(t) }
+  }
+}
+EOF
+
+#include "harness.bash"
