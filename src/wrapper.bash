f088f5eb0a09d288120f9702fcfaafc6d12421be
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-27 21:42:10 -0700

    Port Ubuntu patches to flo-llvm (wrapper)
    These changes are also necessary in order to make the compiler wrapper
    do anything on Ubuntu.

--- src/wrapper.bash
+++ src/wrapper.bash
@@ -1,8 +1,28 @@
+# If we weren't passed an input then just send the help text.
 input="$1"
-
 if [[ "$input" == "" ]]
 then
     input="--help"
+fi
+
+source /etc/lsb-release || true
+
+llvm_link="llvm-link"
+opt="opt"
+llc="llc"
+clang="clang"
+
+# For some crazy reason, Ubuntu has decided to have a clang that's
+# actually incompatible with their installed LLVM tools.  I don't know
+# why anyone would do such a thing... :(.  To avoid all this I just
+# have the Ubuntu package explicitly depend on the 3.3 version of the
+# tools and postfix all names.
+if [[ "$DISTRIB_DESCRIPTION" == "Ubuntu 12.04.4 LTS" ]]
+then
+    llvm_link="llvm-link-3.3"
+    opt="opt-3.3"
+    llc="llc-3.3"
+    clang="clang" # That's right, the clang-3.3 package installs clang!
 fi
 
 if [[ "$input" == "--help" ]]
@@ -19,14 +39,14 @@ flo-llvm-c++ "$input" --header > $tempdir/design.h
 flo-llvm-c++ "$input" --compat > $tempdir/compat.c++
 flo-llvm-c++ "$input" --ir     > $tempdir/design.llvm
 
-clang -c -S -emit-llvm \
+$clang -c -S -emit-llvm \
     -I "$(dirname $input)" \
     -include $tempdir/design.h \
     $tempdir/compat.c++ -o $tempdir/compat.llvm
 
-llvm-link $tempdir/design.llvm $tempdir/compat.llvm > $tempdir/link.llvm
+$llvm_link $tempdir/design.llvm $tempdir/compat.llvm > $tempdir/link.llvm
 
-llc -O2 $tempdir/link.llvm -o $tempdir/opt.S
+$llc -O2 $tempdir/link.llvm -o $tempdir/opt.S
 
 c++ $tempdir/opt.S -c -o $tempdir/opt.o
 
