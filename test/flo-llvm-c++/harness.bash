ea6357fa5e974af40bf75d3de426a5eb28afa1c6
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-27 17:25:44 -0700

    Explicitly support testing on Ubuntu
    This is a bit of a mess: Ubuntu doesn't appear to have a sane
    mechanism for installing clang.  Note that some of the tests still
    fail on my Ubuntu machine, which is probably a Bad Thing...

--- test/flo-llvm-c++/harness.bash
+++ test/flo-llvm-c++/harness.bash
@@ -1,6 +1,26 @@
 set -e
 set -x
 
+source /etc/lsb-release
+
+llvm_link="llvm-link"
+opt="opt"
+llc="llc"
+clang="clang"
+
+# For some crazy reason, Ubuntu has decided to have a clang that's
+# actually incompatible with their installed LLVM tools.  I don't know
+# why anyone would do such a thing... :(.  To avoid all this I just
+# have the Ubuntu package explicitly depend on the 3.3 version of the
+# tools and postfix all names.
+if [[ "$DISTRIB_DESCRIPTION" == "Ubuntu 12.04.4 LTS" ]]
+then
+    llvm_link="llvm-link-3.3"
+    opt="opt-3.3"
+    llc="llc-3.3"
+    clang="clang" # That's right, the clang-3.3 package installs clang!
+fi
+
 # Checks if we've been given a scala file, which means everything can
 # be generated right from here.
 if test -f test.scala
@@ -37,10 +57,10 @@ cat test.h
 time $PTEST_BINARY test.flo --compat > compat.c++
 cat compat.c++
 
-time clang -g -c -std=c++11 harness.c++ -o harness.llvm -S -emit-llvm
+time $clang -g -c -std=c++11 harness.c++ -o harness.llvm -S -emit-llvm
 #cat harness.llvm
 
-time clang -g -c -include test.h -std=c++11 compat.c++ \
+time $clang -g -c -include test.h -std=c++11 compat.c++ \
     -o compat.llvm -S -emit-llvm
 #cat compat.llvm
 
@@ -50,17 +70,17 @@ time $PTEST_BINARY test.flo --ir > test.llvm
 cat test.llvm
 
 # Links together all the bitcode files
-time llvm-link test.llvm compat.llvm harness.llvm -S > exe.llvm
+time $llvm_link test.llvm compat.llvm harness.llvm -S > exe.llvm
 #cat exe.llvm
 
 # Optimizes the assembly that was generated.  I'm not sure if this is
 # necessary to do before I stick in inside the JIT or not...
-time opt -O2 exe.llvm -S > opt.llvm
+time $opt -O2 exe.llvm -S > opt.llvm
 #cat opt.llvm
 
 # Runs the new emulator inside the LLVM interpreter (or probably JIT
 # compiler, if you're using a sane architecture).
-llc opt.llvm -o opt.S
+$llc opt.llvm -o opt.S
 c++ -g opt.S -o opt
 if test -f test.stdin
 then
