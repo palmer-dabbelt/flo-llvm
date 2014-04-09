b9acef4b8413dc4dbfdfcd3b8ecbfb0a1ebeee4d
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-04-09 11:32:16 -0700

    Apparently OSX doesn't support "sed -i"
    Yuck.

--- test/flo-llvm-c++/large_sift.bash
+++ test/flo-llvm-c++/large_sift.bash
@@ -9,10 +9,14 @@ cat >>$TEST.tar.gz.base64 <<EOF
 EOF
 cat $TEST.tar.gz.base64 | base64 --decode | gunzip | tar -x
 
-sed 's/package SIFT//g' -i *.scala
+find . -iname "*.scala" | while read f
+do
+    cat "$f" | sed 's/package SIFT//g' > "$f".sedtmp
+    mv "$f".sedtmp "$f"
+done
 
-sed 's/object SIFT/object ScaleSpaceExtrema/g' -i main.scala
-cat main.scala >> ScaleSpaceExtrema.scala
+cat main.scala | sed 's/object SIFT/object ScaleSpaceExtrema/g' \
+    >> ScaleSpaceExtrema.scala
 rm main.scala
 
 #include "harness.bash"
