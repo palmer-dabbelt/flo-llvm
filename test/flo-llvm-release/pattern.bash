1b5213df8d8c4a4a83e24d1674ca6dec7a21f4a2
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-07-05 12:16:58 -0700

    Add flo-{patterns,torture} tests
    Now I can run all the chisel-torture tests on every commit, which is
    nice.  Unfortunately this appears to depend on chisel-benchmarks,
    which is a huge pain... :(.

---
+++ test/flo-llvm-release/pattern.bash
@@ -0,0 +1,17 @@
+#include "tempdir.bash"
+
+testname="$(basename "$(dirname "$0")")"
+pattern="$(echo "$testname" | cut -d- -f2)"
+args="$(echo "$testname" | cut -d- -f3-)"
+
+TEST="Torture"
+
+flo-patterns --show $pattern $args
+cat Torture.flo
+cat Torture.vcd
+mv Torture.vcd gold.vcd
+
+chisel-hdrtar headers.tar
+tar -xf headers.tar
+
+#include "harness.bash"
---
+++ test/flo-llvm-release/torture.bash
@@ -0,0 +1,16 @@
+#include "tempdir.bash"
+
+testname="$(basename "$(dirname "$0")")"
+seed="$(echo "$testname" | cut -d- -f2)"
+
+TEST="Torture"
+
+flo-torture --seed $seed
+cat Torture.flo
+cat Torture.vcd
+mv Torture.vcd gold.vcd
+
+chisel-hdrtar headers.tar
+tar -xf headers.tar
+
+#include "harness.bash"
